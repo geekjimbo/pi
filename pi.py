adea86b993cb54157f4f15e2a20034b4c73f4904
Jimmy Figueroa <jimmyfigueroa@me.com>
2020-10-09 14:17:54 -0600

    changing pi.py

--- pi.py
+++ pi.py
@@ -3,6 +3,7 @@ from random import random
 from operator import add
 
 from pyspark.sql import SparkSession
+from pyspark import SparkConf
 
 if __name__ == "__main__":
     """
@@ -13,12 +14,12 @@ if __name__ == "__main__":
         .appName("PythonPi")\
         .getOrCreate()
 
-    spark.sparkContext.hadoopConfiguration().set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    spark.sparkContext.hadoopConfiguration().set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-    spark.sparkContext.hadoopConfiguration().set("spark.driver.extraClassPath", "/opt/spark/jars/*")
-    spark.sparkContext.hadoopConfiguration().set("spark.executor.extraClassPath", "/opt/spark/jars/*")
-    spark.sparkContext.hadoopConfiguration().set("fs.s3.awsAccessKeyId", "AKIAWR6Y3LBV4FF42GCK")
-    spark.sparkContext.hadoopConfiguration().set("fs.s3.awsSecretAccessKey", "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")
+    #spark.sparkContext.hadoopConfiguration().set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    #spark.sparkContext.hadoopConfiguration().set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+    #spark.sparkContext.hadoopConfiguration().set("spark.driver.extraClassPath", "/opt/spark/jars/*")
+    #spark.sparkContext.hadoopConfiguration().set("spark.executor.extraClassPath", "/opt/spark/jars/*")
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.awsAccessKeyId", "AKIAWR6Y3LBV4FF42GCK")
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3.awsSecretAccessKey", "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")
 
     myfile = spark.sparkContext.textFile("s3a://pro-pair-serverless/staging/test/myfile.txt")
     myfile.saveAsTextFile("s3a://pro-pair-serverless/staging/test/test/myoutfile.txt")
@@ -32,7 +33,6 @@ if __name__ == "__main__":
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Jimmy was here !!!!")
     print("Pi is roughly %f" % (4.0 * count / n))
     
     spark.stop()
